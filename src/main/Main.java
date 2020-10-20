653755a6f435d714462848bad6226619d5dbaf7b
Juan Francisco <juanf.perez.97@gmail.com>
2020-10-20 01:21:40 -0300

    fix

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,7 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("test asd ahre");
-		System.out.println("test2");
+		System.out.println("test");
 	}
+
 }
