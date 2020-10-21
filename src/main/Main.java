9f3c325450e90d122129cb7524fb94e9ddc0d092
GonzaloCortave <gonza.cortave@hotmail.com>
2020-10-21 14:55:49 -0300

    test de branch

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,8 +3,8 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("test");
-		System.out.println("comit Test6");
+		System.out.println("test2");
+		System.out.println("comit Test40");
 	}
 
 }
