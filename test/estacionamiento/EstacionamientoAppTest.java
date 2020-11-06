6956c390d60d8e79dbdc47031c17aaffd0b05393
GonzaloCortave <gonza.cortave@hotmail.com>
2020-11-06 12:14:25 -0300

    Estacionamiento testeado, se podria ver lo del LocalTime

--- test/estacionamiento/EstacionamientoAppTest.java
+++ test/estacionamiento/EstacionamientoAppTest.java
@@ -1,14 +1,45 @@
-//package estacionamiento;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//import org.junit.jupiter.api.Test;
-//
-//class EstacionamientoAppTest {
-//
-//	@Test
-//	void test() {
-//		fail("Not yet implemented");
-//	}
-//
-//}
+package estacionamiento;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+import java.time.LocalTime;
+
+import org.junit.jupiter.api.Test;
+
+import compras.CompraPuntual;
+
+
+
+
+class EstacionamientoAppTest {
+	EstacionamientoApp estacionamiento;
+	@Test
+	void testConstructor() {
+		
+		estacionamiento = 
+				new EstacionamientoApp("AAA-111",LocalTime.of(10, 0),LocalTime.of(14, 0),"03-03-456");
+		
+	assertTrue(estacionamiento.esSuPatente("AAA-111"));
+	assertTrue(estacionamiento.estaVigente()); 
+	assertEquals(LocalTime.of(10,0), estacionamiento.getHoraInicio());
+	assertEquals(LocalTime.of(14, 0), estacionamiento.getHoraFin());
+	assertTrue(estacionamiento.esSuCelular("03-03-456"));
+	
+		
+	}
+	
+	@Test
+	void cuandoFinaliza() {
+		
+		estacionamiento = 
+				new EstacionamientoApp("AAA-111",LocalTime.of(10, 0),LocalTime.of(14, 0),"03-03-456");
+		assertTrue(estacionamiento.estaVigente());
+		
+		estacionamiento.finalizar();
+		
+		assertFalse(estacionamiento.estaVigente());
+		
+		
+	}
+}
